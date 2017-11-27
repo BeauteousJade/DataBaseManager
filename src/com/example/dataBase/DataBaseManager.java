b7ea4e382d81d53442ce34931138e67b0b1bff4b
coder-farmer <pby2196995023@163.com>
2017-11-27 21:26:17 +0800

    新建了DataBaseManager类

---
+++ src/com/example/dataBase/DataBaseManager.java
@@ -0,0 +1,19 @@
+package com.example.dataBase;
+
+import java.sql.ResultSet;
+
+public interface DataBaseManager {
+	
+	
+	public boolean ceareTable(String tableName, String[] fieldNames, String[] fieldTypes);
+	
+	public boolean insert(String tableName, String[] argNames, String[] argValues);
+	
+	public int update(String tableName, String[] selectArgNames, String [] selectArgValues, String [] updateArgNames, String [] updateArgValues);
+	
+	public boolean delete(String tableName, String [] selectArgNames, String []selectArgValues);
+	
+	public ResultSet query(String tableName, String[] columns, String [] selections, String []selectionArgrs);
+	
+	
+}
